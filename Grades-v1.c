b281e5735132d3000c6c206e1a924c8d1f5865f8
Katapkoon <katapat80@gmail.com>
2022-08-06 16:34:50 +0700

    8/6/2022 4.34 PM

--- Grades-v1.c
+++ Grades-v1.c
@@ -4,7 +4,15 @@ int main()
 {
     float score;
     scanf("%f",&score);
-    if(score >= 90)
+    if(score < 0)
+    {
+        printf("Error : Value must be greater than or equal to 0."); 
+    }
+    else if(score > 100)
+    {
+        printf("Error : Value must be less than or equal to 100.");
+    }
+    else if(score >= 90)
     {
         char grade[10] = "A";
         printf("%s", grade);
@@ -39,7 +47,7 @@ int main()
         char grade[10]  = "D";
         printf("%s", grade);   
     }
-    else
+    else if(score < 60 || score > 0)
     {
         char grade[10]  = "F";
         printf("%s", grade);  
